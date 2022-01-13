2603257c85348cd94ba270bf585dd3f5455d82c8
Egor Uvarov <52317049+josephRTi@users.noreply.github.com>
2022-01-13 15:34:42 +0300

    final

---
+++ src/components/TheIndex.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TheIndex"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheLogin.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TheLogin"
+}
+</script>
+
+<style scoped>
+
+</style>
