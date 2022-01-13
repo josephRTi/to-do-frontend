72cbdea45a9317c9904403677c93096d00123f11
Egor Uvarov <52317049+josephRTi@users.noreply.github.com>
2022-01-13 16:13:27 +0300

    ver 1.0 fixes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(cors({
+    origin: '*'
+}));
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log('Listening on port: ' + port)
